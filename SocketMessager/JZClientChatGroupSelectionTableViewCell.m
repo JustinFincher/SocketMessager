c07e5c0729ddb552ff0c3e09dbfde63310d4bb47
Fincher Justin <zhtsu47@me.com>
2016-06-13 07:15:46 +0800

    ..

--- SocketMessager/JZClientChatGroupSelectionTableViewCell.m
+++ SocketMessager/JZClientChatGroupSelectionTableViewCell.m
@@ -13,6 +13,8 @@
 - (void)awakeFromNib {
     [super awakeFromNib];
     // Initialization code
+    self.contentView.backgroundColor = [UIColor clearColor];
+    self.backgroundView.backgroundColor = [UIColor clearColor];
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
