34c4b1822c96b041661ba205acad7b6be752f2ed
Fincher Justin <zhtsu47@me.com>
2016-06-13 04:13:08 +0800

    small tweak.

--- SocketMessager/JZClientChatGroupSelectionViewController.m
+++ SocketMessager/JZClientChatGroupSelectionViewController.m
@@ -8,21 +8,27 @@
 
 #import "JZClientChatGroupSelectionViewController.h"
 #import "JZSocketManager.h"
-
-@interface JZClientChatGroupSelectionViewController ()<JZSocketManagerDelegate>
+#import "JZClientChatGroupSelectionTableViewCell.h"
+@interface JZClientChatGroupSelectionViewController ()<JZSocketManagerDelegate,UITableViewDelegate,UITableViewDataSource>
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (weak, nonatomic) IBOutlet UILabel *onlineDevicesLabel;
+
+@property (strong,nonatomic) NSArray *addressArray;
 
 @end
 
 @implementation JZClientChatGroupSelectionViewController
-@synthesize onlineDevicesLabel;
+@synthesize onlineDevicesLabel,addressArray;
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
     JZSocketManager *socketManger = [JZSocketManager sharedManager];
     socketManger.delegate = self;
     // Do any additional setup after loading the view.
+    
+    self.tableView.allowsMultipleSelection = YES;
+    [self.tableView registerNib:[UINib nibWithNibName:@"JZClientChatGroupSelectionTableViewCell" bundle:nil] forCellReuseIdentifier:@"JZClientChatGroupSelectionTableViewCell"];
 }
 
 #pragma mark - JZSocketManagerDelegate
@@ -30,8 +36,9 @@
 {
     if (tag == TAG_DEVICEARRAY)
     {
-        NSArray *array = [NSKeyedUnarchiver unarchiveObjectWithData:data];
-        onlineDevicesLabel.text = [NSString stringWithFormat:@"%lu DEVEICES ONLINE",(unsigned long)[array count]];
+        addressArray = [NSKeyedUnarchiver unarchiveObjectWithData:data];
+        onlineDevicesLabel.text = [NSString stringWithFormat:@"%lu DEVEICES ONLINE",(unsigned long)[addressArray count]];
+        [self reloadTableView];
     }
 }
 
@@ -42,6 +49,38 @@
 - (BOOL)prefersStatusBarHidden
 {
     return YES;
+}
+#pragma mark - Table view
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    if (addressArray)
+    {
+        return [addressArray count];
+    }else
+    {
+        return 0;
+    }
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    JZClientChatGroupSelectionTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"JZClientChatGroupSelectionTableViewCell" forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    [cell setLabelText:[addressArray objectAtIndex:indexPath.row]];
+    return cell;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 64.0f;
+}
+- (void)reloadTableView
+{
+    NSArray *indexPaths = [self.tableView indexPathsForSelectedRows];
+    [self.tableView reloadData];
+    for (NSIndexPath *path in indexPaths) {
+        [self.tableView selectRowAtIndexPath:path animated:NO scrollPosition:UITableViewScrollPositionNone];
+    }
 }
 
 /*
